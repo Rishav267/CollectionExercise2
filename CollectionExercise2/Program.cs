1492ed08427cd170ce83d72db7dfb952d47bae83
Rishav Kedia <rishav267@gmail.com>
2021-07-17 10:46:00 +0530

    Add project files.

---
+++ CollectionExercise2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            for (int i = 0; i < args.Length; i++)
+            {
+                dict[args[i]] = args[i].Length;
+
+                //if (dict.ContainsKey(args[i]))
+                //{
+                //    dict[args[i]]++;
+                //}
+                //else
+                //{
+                //    dict[args[i]] = 1;
+                //}
+            }
+            string str = " ";
+            //dict = dict.OrderBy(Key => Key.Key);
+            foreach (KeyValuePair<string, int> k in dict.OrderBy(Key => Key.Value))
+            {
+                str += " ";
+                str += k.Key;
+                // Console.WriteLine("key = {0}, Value = {1}", k.Key, k.Value);               
+                
+            }
+            Console.WriteLine("sorted string is: " + str);
+        }
+    }
+}
